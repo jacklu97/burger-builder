483e5fb255ef0ace5637baa727b118d59ed11794
Luis Angel Hernández <jacklu@pop-os.localdomain>
2020-07-30 21:03:18 -0500

    SideDrawer implementation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../../components/Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerTogge from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolbar = (props) => {
     return ( 
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo />
-            <nav>
-                ...
+            <DrawerTogge clicked={props.drawerToggleClicked}/>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav className={classes.DesktopOnly}>
+                <NavigationItems />
             </nav>
         </header>
      );
