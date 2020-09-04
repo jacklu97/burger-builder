50085d7410f19f5db57a34d4ce2cf905756da433
Luis Angel Hernández <jacklu@pop-os.localdomain>
2020-09-04 14:17:58 -0500

    Persistant data and route guards

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,6 +24,9 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -50,8 +53,12 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response)
-                dispatch(authSuccess(response.data.idToken, response.data.localId))
-                dispatch(checkAuthTimeOut(response.data.expiresIn))
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('userId', response.data.localId)
+                localStorage.setItem('expirationDate', expirationDate);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeOut(response.data.expiresIn));
             })
             .catch(err => {
                 console.log(err)
@@ -64,5 +71,23 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token){
+            dispatch(logout())
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'))
+            if(expirationDate <= new Date()){
+                dispatch(logout())
+            }
+            else{
+                dispatch(authSuccess(token, localStorage.getItem('userId')))
+                dispatch(checkAuthTimeOut( (expirationDate.getTime() - new Date().getTime()) / 1000 ) )
+            }
+        }
     }
 }
